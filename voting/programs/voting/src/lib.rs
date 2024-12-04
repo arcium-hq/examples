32801b9b2e8c06163949d2b8932c52db761c59e1
Arihant Bansal <17180950+arihantbansal@users.noreply.github.com>
2024-12-05 02:25:52 +0530

    init

---
+++ voting/programs/voting/src/lib.rs
@@ -0,0 +1,393 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token, TokenAccount};
+use arcium_anchor::{
+    comp_def_offset, derive_seed, init_comp_def, init_da_object, queue_computation,
+    ARCIUM_TOKEN_MINT,
+};
+use arcium_client::idl::arcium::{
+    accounts::{
+        ClockAccount, Cluster, ComputationDefinitionAccount, DataObjectAccount, Mempool,
+        PersistentMXEAccount, StakingPoolAccount,
+    },
+    program::Arcium,
+    types::OffChainReference,
+    ID_CONST as ARCIUM_PROG_ID,
+};
+use arcium_macros::{
+    arcium_callback, arcium_program, callback_accounts, init_computation_definition_accounts,
+    init_data_object_accounts, queue_computation_accounts,
+};
+
+const MXE_PDA_SEED: &'static [u8] = derive_seed!(PersistentMXEAccount);
+const DA_OBJ_PDA_SEED: &'static [u8] = derive_seed!(DataObjectAccount);
+const MEMPOOL_PDA_SEED: &'static [u8] = derive_seed!(Mempool);
+const COMP_DEF_PDA_SEED: &'static [u8] = derive_seed!(ComputationDefinitionAccount);
+const CLUSTER_PDA_SEED: &'static [u8] = derive_seed!(Cluster);
+const POOL_PDA_SEED: &'static [u8] = derive_seed!(StakingPoolAccount);
+const CLOCK_PDA_SEED: &'static [u8] = derive_seed!(ClockAccount);
+const COMP_DEF_OFFSET_VOTE: u32 = comp_def_offset("vote");
+const COMP_DEF_OFFSET_REVEAL: u32 = comp_def_offset("reveal_result");
+
+declare_id!("6efqLVrBAanYujaKqp7b93VWk7YsRhLAo8ERihjuG6EY");
+
+#[arcium_program]
+pub mod voting {
+    use super::*;
+
+    pub fn create_new_poll(
+        ctx: Context<CreateNewPoll>,
+        id: u32,
+        question: String,
+        initial_vote_state: OffChainReference,
+    ) -> Result<()> {
+        msg!("Creating a new poll");
+
+        init_da_object(
+            ctx.accounts,
+            initial_vote_state,
+            ctx.accounts.vote_state.to_account_info(),
+            id,
+        )?;
+
+        ctx.accounts.poll_acc.question = question;
+        ctx.accounts.poll_acc.vote_state_da_obj = ctx.accounts.vote_state.key();
+        ctx.accounts.poll_acc.bump = ctx.bumps.poll_acc;
+        ctx.accounts.poll_acc.id = id;
+        ctx.accounts.poll_acc.authority = ctx.accounts.payer.key();
+
+        Ok(())
+    }
+
+    pub fn init_vote_comp_def(ctx: Context<InitVoteCompDef>) -> Result<()> {
+        init_comp_def(ctx.accounts)?;
+        Ok(())
+    }
+
+    pub fn vote(ctx: Context<Vote>, id: u32, vote_state: OffChainReference) -> Result<()> {
+        queue_computation(
+            ctx.accounts,
+            Some(vote_state),
+            vec![id],
+            vec![ctx.accounts.vote_state.to_account_info()],
+            vec![],
+        )?;
+        Ok(())
+    }
+
+    #[arcium_callback(confidential_ix = "vote")]
+    pub fn vote_callback(ctx: Context<VoteCallback>, output: Vec<u8>) -> Result<()> {
+        msg!("Arcium callback invoked with output {:?}", output);
+        Ok(())
+    }
+
+    pub fn init_reveal_result_comp_def(ctx: Context<InitRevealResultCompDef>) -> Result<()> {
+        init_comp_def(ctx.accounts)?;
+        Ok(())
+    }
+
+    pub fn reveal_result(
+        ctx: Context<RevealVotingResult>,
+        id: u32,
+        vote_state: OffChainReference,
+    ) -> Result<()> {
+        require!(
+            ctx.accounts.payer.key() == ctx.accounts.poll_acc.authority,
+            ErrorCode::InvalidAuthority
+        );
+
+        msg!(
+            "Revealing voting result for poll with id {}",
+            ctx.accounts.poll_acc.id
+        );
+
+        queue_computation(
+            ctx.accounts,
+            Some(vote_state),
+            vec![ctx.accounts.poll_acc.id],
+            vec![ctx.accounts.vote_state.to_account_info()],
+            vec![],
+        )?;
+        Ok(())
+    }
+
+    #[arcium_callback(confidential_ix = "reveal_result")]
+    pub fn reveal_result_callback(
+        ctx: Context<RevealVotingResultCallback>,
+        output: Vec<u8>,
+    ) -> Result<()> {
+        msg!("Arcium callback invoked with output {:?}", output);
+        Ok(())
+    }
+}
+
+#[init_data_object_accounts(payer)]
+#[instruction(id: u32)]
+pub struct CreateNewPoll<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(
+        init,
+        payer = payer,
+        space = 8 + PollAccount::INIT_SPACE,
+        seeds = [b"poll", payer.key().as_ref(), id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub poll_acc: Account<'info, PollAccount>,
+    /// CHECK:
+    #[account(mut)]
+    pub vote_state: UncheckedAccount<'info>,
+    #[account(
+        mut,
+        seeds = [MXE_PDA_SEED, ID_CONST.to_bytes().as_ref()],
+        seeds::program = ARCIUM_PROG_ID,
+        bump = mxe.bump
+    )]
+    pub mxe: Account<'info, PersistentMXEAccount>,
+    pub arcium_program: Program<'info, Arcium>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
+
+#[queue_computation_accounts("vote", payer)]
+#[instruction(id: u32)]
+pub struct Vote<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(
+        mut,
+        associated_token::mint = arx_mint,
+        associated_token::authority = payer,
+    )]
+    pub payer_ata: Account<'info, TokenAccount>,
+    #[account(address = ARCIUM_TOKEN_MINT)]
+    pub arx_mint: Account<'info, Mint>,
+    #[account(
+        seeds = [MXE_PDA_SEED, ID.to_bytes().as_ref()],
+        seeds::program = ARCIUM_PROG_ID,
+        bump = mxe_account.bump
+    )]
+    pub mxe_account: Account<'info, PersistentMXEAccount>,
+    #[account(
+        mut,
+        seeds = [MEMPOOL_PDA_SEED, ID.to_bytes().as_ref()],
+        seeds::program = ARCIUM_PROG_ID,
+        bump = mempool_account.bump
+    )]
+    pub mempool_account: Account<'info, Mempool>,
+    #[account(
+        seeds = [COMP_DEF_PDA_SEED, COMP_DEF_OFFSET_VOTE.to_le_bytes().as_ref()],
+        seeds::program = ARCIUM_PROG_ID,
+        bump = comp_def_account.bump
+    )]
+    pub comp_def_account: Account<'info, ComputationDefinitionAccount>,
+    #[account(
+        mut,
+        // TODO: Replace with actual cluster offset
+        seeds = [CLUSTER_PDA_SEED, 0u32.to_le_bytes().as_ref()],
+        seeds::program = ARCIUM_PROG_ID,
+        bump = cluster_account.bump
+    )]
+    pub cluster_account: Account<'info, Cluster>,
+    #[account(
+        mut,
+        seeds = [POOL_PDA_SEED],
+        seeds::program = ARCIUM_PROG_ID,
+        bump = pool_account.bump
+    )]
+    pub pool_account: Account<'info, StakingPoolAccount>,
+    #[account(
+        mut,
+        associated_token::mint = arx_mint,
+        associated_token::authority = pool_account,
+    )]
+    pub pool_ata: Account<'info, TokenAccount>,
+    #[account(
+        seeds = [CLOCK_PDA_SEED],
+        seeds::program = ARCIUM_PROG_ID,
+        bump = clock_account.bump
+    )]
+    pub clock_account: Account<'info, ClockAccount>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub arcium_program: Program<'info, Arcium>,
+    /// CHECK: Poll authority pubkey
+    pub authority: UncheckedAccount<'info>,
+    #[account(
+        seeds = [b"poll", authority.key().as_ref(), id.to_le_bytes().as_ref()],
+        bump = poll_acc.bump,
+        has_one = authority,
+    )]
+    pub poll_acc: Account<'info, PollAccount>,
+    #[account(
+        seeds = [DA_OBJ_PDA_SEED, id.to_le_bytes().as_ref()],
+        seeds::program = ARCIUM_PROG_ID,
+        owner = ARCIUM_PROG_ID,
+        bump = vote_state.bump,
+    )]
+    pub vote_state: Account<'info, DataObjectAccount>,
+}
+
+#[callback_accounts("vote", payer)]
+pub struct VoteCallback<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub arcium_program: Program<'info, Arcium>,
+    #[account(
+        seeds = [COMP_DEF_PDA_SEED, COMP_DEF_OFFSET_VOTE.to_le_bytes().as_ref()],
+        seeds::program = ARCIUM_PROG_ID,
+        bump = comp_def_account.bump
+    )]
+    pub comp_def_account: Account<'info, ComputationDefinitionAccount>,
+    #[account(address = ::anchor_lang::solana_program::sysvar::instructions::ID)]
+    pub instructions_sysvar: AccountInfo<'info>,
+}
+
+#[init_computation_definition_accounts("vote", payer)]
+pub struct InitVoteCompDef<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(
+        address = ID_CONST
+    )]
+    pub mxe: AccountInfo<'info>,
+    #[account(
+        mut,
+        seeds = [MXE_PDA_SEED, ID_CONST.to_bytes().as_ref()],
+        seeds::program = ARCIUM_PROG_ID,
+        bump = mxe_acc.bump
+    )]
+    pub mxe_acc: Box<Account<'info, PersistentMXEAccount>>,
+    #[account(mut)]
+    pub comp_def_acc: UncheckedAccount<'info>,
+    pub arcium_program: Program<'info, Arcium>,
+    pub system_program: Program<'info, System>,
+}
+
+#[queue_computation_accounts("reveal_result", payer)]
+#[instruction(id: u32)]
+pub struct RevealVotingResult<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(
+        seeds = [b"poll", poll_acc.authority.key().as_ref(), id.to_le_bytes().as_ref()],
+        bump = poll_acc.bump
+    )]
+    pub poll_acc: Account<'info, PollAccount>,
+    #[account(
+        mut,
+        associated_token::mint = arx_mint,
+        associated_token::authority = payer,
+    )]
+    pub payer_ata: Account<'info, TokenAccount>,
+    #[account(address = ARCIUM_TOKEN_MINT)]
+    pub arx_mint: Account<'info, Mint>,
+    #[account(
+        seeds = [MXE_PDA_SEED, ID.to_bytes().as_ref()],
+        seeds::program = ARCIUM_PROG_ID,
+        bump = mxe_account.bump
+    )]
+    pub mxe_account: Account<'info, PersistentMXEAccount>,
+    #[account(
+        mut,
+        seeds = [MEMPOOL_PDA_SEED, ID.to_bytes().as_ref()],
+        seeds::program = ARCIUM_PROG_ID,
+        bump = mempool_account.bump
+    )]
+    pub mempool_account: Account<'info, Mempool>,
+    #[account(
+        seeds = [COMP_DEF_PDA_SEED, COMP_DEF_OFFSET_REVEAL.to_le_bytes().as_ref()],
+        seeds::program = ARCIUM_PROG_ID,
+        bump = comp_def_account.bump
+    )]
+    pub comp_def_account: Account<'info, ComputationDefinitionAccount>,
+    #[account(
+        mut,
+        // TODO: Replace with actual cluster offset
+        seeds = [CLUSTER_PDA_SEED, 0u32.to_le_bytes().as_ref()],
+        seeds::program = ARCIUM_PROG_ID,
+        bump = cluster_account.bump
+    )]
+    pub cluster_account: Account<'info, Cluster>,
+    #[account(
+        mut,
+        seeds = [POOL_PDA_SEED],
+        seeds::program = ARCIUM_PROG_ID,
+        bump = pool_account.bump
+    )]
+    pub pool_account: Account<'info, StakingPoolAccount>,
+    #[account(
+        mut,
+        associated_token::mint = arx_mint,
+        associated_token::authority = pool_account,
+    )]
+    pub pool_ata: Account<'info, TokenAccount>,
+    #[account(
+        seeds = [CLOCK_PDA_SEED],
+        seeds::program = ARCIUM_PROG_ID,
+        bump = clock_account.bump
+    )]
+    pub clock_account: Account<'info, ClockAccount>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub arcium_program: Program<'info, Arcium>,
+    #[account(
+        seeds = [DA_OBJ_PDA_SEED, poll_acc.authority.key().as_ref(), poll_acc.id.to_le_bytes().as_ref()],
+        seeds::program = ARCIUM_PROG_ID,
+        owner = ARCIUM_PROG_ID,
+        bump = vote_state.bump,
+    )]
+    pub vote_state: Account<'info, DataObjectAccount>,
+}
+
+#[callback_accounts("reveal_result", payer)]
+pub struct RevealVotingResultCallback<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub arcium_program: Program<'info, Arcium>,
+    #[account(
+        seeds = [COMP_DEF_PDA_SEED, COMP_DEF_OFFSET_REVEAL.to_le_bytes().as_ref()],
+        seeds::program = ARCIUM_PROG_ID,
+        bump = comp_def_account.bump
+    )]
+    pub comp_def_account: Account<'info, ComputationDefinitionAccount>,
+    #[account(address = ::anchor_lang::solana_program::sysvar::instructions::ID)]
+    pub instructions_sysvar: AccountInfo<'info>,
+}
+
+#[init_computation_definition_accounts("reveal_result", payer)]
+pub struct InitRevealResultCompDef<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(
+        address = ID_CONST
+    )]
+    pub mxe: AccountInfo<'info>,
+    #[account(
+        mut,
+        seeds = [MXE_PDA_SEED, ID_CONST.to_bytes().as_ref()],
+        seeds::program = ARCIUM_PROG_ID,
+        bump = mxe_acc.bump
+    )]
+    pub mxe_acc: Box<Account<'info, PersistentMXEAccount>>,
+    #[account(mut)]
+    pub comp_def_acc: UncheckedAccount<'info>,
+    pub arcium_program: Program<'info, Arcium>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct PollAccount {
+    #[max_len(50)]
+    pub question: String,
+    pub id: u32,
+    pub authority: Pubkey,
+    pub vote_state_da_obj: Pubkey,
+    pub bump: u8,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Invalid authority")]
+    InvalidAuthority,
+}
---
+++ voting/tests/voting.ts
@@ -0,0 +1,239 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
+import { Voting } from "../target/types/voting";
+import { Vote } from "../confidential-ixs/build/vote";
+import {
+  ConfidentialInstructionInputs,
+  getClusterDAInfo,
+  getArciumEnv,
+  buildOffchainRefRequest,
+  DANodeClient,
+  getCompDefAccOffset,
+  getArciumAccountBaseSeed,
+  getArciumProgAddress,
+  uploadCircuit,
+  buildFinalizeCompDefTx,
+  trackComputationProgress,
+  getDataObjPDA,
+  MBoolean,
+} from "@elusiv-privacy/arcium-sdk";
+import * as fs from "fs";
+import * as os from "os";
+import { RevealResult } from "../confidential-ixs/build/reveal_result";
+
+describe("Voting", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+  const program = anchor.workspace.Voting as Program<Voting>;
+  const provider = anchor.getProvider();
+
+  const arciumEnv = getArciumEnv();
+  const daNodeClient = new DANodeClient(arciumEnv.DANodeURL);
+
+  it("Is initialized and can create new polls!", async () => {
+    const POLL_ID = 420;
+    const owner = readKpJson(`${os.homedir()}/.config/solana/id.json`);
+
+    console.log(owner.publicKey.toBase58());
+    console.log("Initializing voting computation definition");
+    const initVoteSig = await initVoteCompDef(program, owner, false);
+    console.log(
+      "Vote computation definition initialized with signature",
+      initVoteSig
+    );
+
+    console.log("Initializing reveal result computation definition");
+    const initRRSig = await initRRCompDef(program, owner, false);
+    console.log(
+      "Reveal result computation definition initialized with signature",
+      initRRSig
+    );
+
+    const pollSig = await createNewPoll(program, daNodeClient, POLL_ID);
+    console.log("Poll created with signature", pollSig);
+
+    // Vote on the poll
+    const inputVal: ConfidentialInstructionInputs<Vote> = [
+      {
+        value: true as MBoolean,
+      },
+      {
+        offset: 0,
+        isMutable: true,
+      },
+    ];
+    const cluster_da_info = await getClusterDAInfo(
+      provider.connection,
+      arciumEnv.arciumClusterPubkey
+    );
+    const req = buildOffchainRefRequest(inputVal, cluster_da_info);
+    const oref = await daNodeClient.postOffchainReference(req);
+    console.log("Built offchain request");
+    const queueSig = await program.methods
+      .vote(POLL_ID, oref)
+      .accounts({
+        authority: owner.publicKey,
+      })
+      .rpc({ commitment: "confirmed" });
+    console.log("Voting queue sig is ", queueSig);
+
+    // const finalizeSig = await trackComputationProgress(
+    //   provider.connection,
+    //   queueSig,
+    //   program.programId,
+    //   "confirmed"
+    // );
+    // console.log("Finalize sig is ", finalizeSig);
+
+    // // Reveal the result
+    // const revealInput: ConfidentialInstructionInputs<RevealResult> = [
+    //   {
+    //     offset: 0,
+    //     isMutable: true,
+    //   },
+    // ];
+    // const revealReq = buildOffchainRefRequest(inputVal, cluster_da_info);
+    // const revealOref = await daNodeClient.postOffchainReference(req);
+
+    // const revealQueueSig = await program.methods
+    //   .revealResult(POLL_ID, revealOref)
+    //   .accounts({})
+    //   .rpc({ commitment: "confirmed" });
+    // console.log("Reveal queue sig is ", revealQueueSig);
+    // const revealFinalizeSig = await trackComputationProgress(
+    //   provider.connection,
+    //   revealQueueSig,
+    //   program.programId,
+    //   "confirmed"
+    // );
+    // console.log("Reveal finalize sig is ", revealFinalizeSig);
+  });
+
+  async function initVoteCompDef(
+    program: Program<Voting>,
+    payer: anchor.web3.Keypair,
+    uploadRawCircuit: boolean
+  ): Promise<string> {
+    const baseSeedCompDefAcc = getArciumAccountBaseSeed(
+      "ComputationDefinitionAccount"
+    );
+    const offset = getCompDefAccOffset("vote");
+
+    const compDefPDA = PublicKey.findProgramAddressSync(
+      [baseSeedCompDefAcc, offset],
+      getArciumProgAddress()
+    )[0];
+
+    const sig = await program.methods
+      .initVoteCompDef()
+      .accounts({ compDefAcc: compDefPDA, payer: payer.publicKey })
+      .signers([payer])
+      .rpc({
+        commitment: "confirmed",
+      });
+    console.log("Init vote computation definition transaction", sig);
+
+    if (uploadRawCircuit) {
+      const rawCircuit = fs.readFileSync("confidential-ixs/build/vote.arcis");
+      await uploadCircuit(provider.connection, payer, "vote", rawCircuit, true);
+    } else {
+      const finalizeTx = await buildFinalizeCompDefTx(
+        payer.publicKey,
+        Buffer.from(offset).readUInt32LE()
+      );
+      const latestBlockhash = await provider.connection.getLatestBlockhash();
+      finalizeTx.recentBlockhash = latestBlockhash.blockhash;
+      finalizeTx.lastValidBlockHeight = latestBlockhash.lastValidBlockHeight;
+      finalizeTx.sign(payer);
+      await provider.sendAndConfirm(finalizeTx);
+    }
+    return sig;
+  }
+
+  async function initRRCompDef(
+    program: Program<Voting>,
+    payer: anchor.web3.Keypair,
+    uploadRawCircuit: boolean
+  ): Promise<string> {
+    const baseSeedCompDefAcc = getArciumAccountBaseSeed(
+      "ComputationDefinitionAccount"
+    );
+    const offset = getCompDefAccOffset("reveal_result");
+
+    const compDefPDA = PublicKey.findProgramAddressSync(
+      [baseSeedCompDefAcc, offset],
+      getArciumProgAddress()
+    )[0];
+
+    const sig = await program.methods
+      .initRevealResultCompDef()
+      .accounts({ compDefAcc: compDefPDA, payer: payer.publicKey })
+      .signers([payer])
+      .rpc({
+        commitment: "confirmed",
+      });
+    console.log("Init reveal_result computation definition transaction", sig);
+
+    if (uploadRawCircuit) {
+      const rawCircuit = fs.readFileSync(
+        "confidential-ixs/build/reveal_result.arcis"
+      );
+      await uploadCircuit(
+        provider.connection,
+        payer,
+        "reveal_result",
+        rawCircuit,
+        true
+      );
+    } else {
+      const finalizeTx = await buildFinalizeCompDefTx(
+        payer.publicKey,
+        Buffer.from(offset).readUInt32LE()
+      );
+      const latestBlockhash = await provider.connection.getLatestBlockhash();
+      finalizeTx.recentBlockhash = latestBlockhash.blockhash;
+      finalizeTx.lastValidBlockHeight = latestBlockhash.lastValidBlockHeight;
+      finalizeTx.sign(payer);
+      await provider.sendAndConfirm(finalizeTx);
+    }
+    return sig;
+  }
+
+  async function createNewPoll(
+    program: Program<Voting>,
+    daNodeClient: DANodeClient,
+    pollId: number
+  ): Promise<string> {
+    const votePDA = getDataObjPDA(getArciumProgAddress(), pollId);
+
+    // Empty vote stats is 2 scalars
+    const emptyVoteState: [{ value: bigint }, { value: bigint }] = [
+      {
+        value: BigInt(0),
+      },
+      {
+        value: BigInt(0),
+      },
+    ];
+
+    const cluster_da_info = await getClusterDAInfo(
+      provider.connection,
+      arciumEnv.arciumClusterPubkey
+    );
+    const req = buildOffchainRefRequest(emptyVoteState, cluster_da_info);
+    const oref = await daNodeClient.postOffchainReference(req);
+
+    return program.methods
+      .createNewPoll(pollId, "$SOL to 500?", oref)
+      .accounts({ voteState: votePDA })
+      .rpc();
+  }
+});
+
+function readKpJson(path: string): anchor.web3.Keypair {
+  const file = fs.readFileSync(path);
+  return anchor.web3.Keypair.fromSecretKey(
+    new Uint8Array(JSON.parse(file.toString()))
+  );
+}
