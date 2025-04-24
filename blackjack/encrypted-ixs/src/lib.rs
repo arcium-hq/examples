acee8a0f5f514993d09e857c544d31c623f196c4
Arihant Bansal <17180950+arihantbansal@users.noreply.github.com>
2025-04-24 12:47:28 +0200

    doesnt work

--- blackjack/encrypted-ixs/src/lib.rs
+++ blackjack/encrypted-ixs/src/lib.rs
@@ -88,15 +88,11 @@ mod circuits {
     }
 
     #[instruction]
-    pub fn generate_deck_of_shuffled_cards(mxe: Mxe) -> bool
-    // -> Enc<Mxe, Deck>
-    {
+    pub fn generate_deck_of_shuffled_cards(mxe: Mxe) -> Enc<Mxe, Deck> {
         let mut deck = INITIAL_DECK;
         ArcisRNG::shuffle(&mut deck);
 
-        // mxe.from_arcis(Deck::from_array(deck))
-
-        true
+        mxe.from_arcis(Deck::from_array(deck))
     }
 
     #[instruction]
